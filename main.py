7c5e5f495a2cf6c7a41885993e06795968c95c3d
Matt-Szymczyk <53269931+Matt-Szymczyk@users.noreply.github.com>
2023-01-20 01:26:22 +0100

    Dopasowanie nuty

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ class TunerApp(tk.Tk):
             frames_per_buffer=self.chunk
         )
 
-        self.label = tk.Label(self, text="Tuning...")
-        self.label.pack()
+        self.note_label = tk.Label(self, text="Tuning...")
+        self.note_label.pack()
+
+        self.frequency_label = tk.Label(self, text="Frequency:")
+        self.frequency_label.pack()
 
         self.update()
 
@@ -27,8 +30,27 @@ class TunerApp(tk.Tk):
         data = np.fromstring(self.stream.read(self.chunk), dtype=np.int16)
         fft_data = np.abs(fft(data))[:self.chunk//2]
         peak_frequency = np.argmax(fft_data) * self.sample_rate / self.chunk
-        self.label.config(text="Frequency: {:.2f}Hz".format(peak_frequency))
+        self.frequency_label.config(text="Frequency: {:.2f}Hz".format(peak_frequency))
+
+        note, error = self.note_from_frequency(peak_frequency)
+        self.note_label.config(text="Note: {} (error: {:.2f} cents)".format(note, error))
         self.after(50, self.update)
+
+    def note_from_frequency(self, frequency):
+        """
+        Returns the nearest note and the error (in cents)
+        for a given frequency.
+        """
+        notes = ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#"]
+        note_frequencies = [82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56]
+        error = 1000000
+        note = ""
+        for i, freq in enumerate(note_frequencies):
+            if abs(frequency - freq) < error:
+                error = abs(frequency - freq)
+                note = notes[i]
+        error = error / freq * 1200
+        return note, error
 
 if __name__ == "__main__":
     app = TunerApp()
