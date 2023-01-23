3f03c2528614c84d840ea9979f6da41fdfae8b05
Matt-Szymczyk <53269931+Matt-Szymczyk@users.noreply.github.com>
2023-01-23 02:22:14 +0100

    Skalowanie

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ import math
 import tkinter as tk
 import customtkinter as ctk
 
-ctk.set_widget_scaling(1.5)  # widget dimensions and text size
-# ctk.set_spacing_scaling(1.5)  # padding and place positions
-ctk.set_window_scaling(1.5)  # window geometry dimensions
+# ctk.set_widget_scaling(1.5)  # widget dimensions and text size
+# # ctk.set_spacing_scaling(1.5)  # padding and place positions
+# ctk.set_window_scaling(1.5)  # window geometry dimensions
 
-SAMPLE_FREQ = 48000 # sample frequency in Hz
-WINDOW_SIZE = 48000 # window size of the DFT in samples
-WINDOW_STEP = 12000 # step size of window
-NUM_HPS = 5 # max number of harmonic product spectrums
-POWER_THRESH = 1e-6 # tuning is activated if the signal power exceeds this threshold
-CONCERT_PITCH = 440 # defining a1
-WHITE_NOISE_THRESH = 0.2 # everything under WHITE_NOISE_THRESH*avg_energy_per_freq is cut off
+SAMPLE_FREQ = 48000  # sample frequency in Hz
+WINDOW_SIZE = 48000  # window size of the DFT in samples
+WINDOW_STEP = 12000  # step size of window
+NUM_HPS = 5  # max number of harmonic product spectrums
+POWER_THRESH = 1e-6  # tuning is activated if the signal power exceeds this threshold
+CONCERT_PITCH = 440  # defining a1
+WHITE_NOISE_THRESH = 0.2  # everything under WHITE_NOISE_THRESH*avg_energy_per_freq is cut off
 
-WINDOW_T_LEN = WINDOW_SIZE / SAMPLE_FREQ # length of the window in seconds
-SAMPLE_T_LENGTH = 1 / SAMPLE_FREQ # length between two samples in seconds
+WINDOW_T_LEN = WINDOW_SIZE / SAMPLE_FREQ  # length of the window in seconds
+SAMPLE_T_LENGTH = 1 / SAMPLE_FREQ  # length between two samples in seconds
 DELTA_FREQ = SAMPLE_FREQ / WINDOW_SIZE
 OCTAVE_BANDS = [50, 100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600]
 
-
-ALL_NOTES = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
+ALL_NOTES = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
 
 HANN_WINDOW = np.hanning(WINDOW_SIZE)
+
 
 class TunerApp:
     def __init__(self, master):
@@ -39,51 +39,91 @@ class TunerApp:
 
         self.tabview = ctk.CTkTabview(master, width=250)
         # self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.tabview.add("Tuner")
-        self.tabview.add("Settings")
-        self.tabview.add("Tab 3")
+
+        self.tuner_tab = self.tabview.add("Tuner")
+        self.settings_tab = self.tabview.add("Settings")
+        self.tabview.set("Tuner")
         self.tabview.pack(padx=20, pady=20)
 
-        self.closest_note_label = ctk.CTkLabel(self.tabview.tab("Tuner"), text="Closest note:")
-        self.closest_note_label.pack()
+        self.scaling_label = ctk.CTkLabel(self.settings_tab, text="Theme:", anchor="w")
+        self.scaling_label.grid(row=0, column=0, padx=(0, 20), pady=(0, 20), sticky="nsew")
+        self.appearance_mode_optionemenu = ctk.CTkOptionMenu(self.settings_tab,
+                                                             values=["System", "Light", "Dark"],
+                                                             command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=0, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.appearance_mode_optionemenu.set("System")
 
+        self.scaling_label = ctk.CTkLabel(self.settings_tab, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scaling_optionemenu = ctk.CTkOptionMenu(self.settings_tab,
+                                                     values=["75%", "100%", "125%", "150%", "175%", "200%"],
+                                                     command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scaling_optionemenu.set("100%")
+        self.scale_factor = 1
+
+        # Closest note
+        self.closest_note_label = ctk.CTkLabel(self.tuner_tab, text="Closest note:")
+        self.closest_note_label.pack()
+        # Closest note value
         self.closest_note_var = tk.StringVar()
         self.closest_note_var.set("...")
-        self.closest_note_display = ctk.CTkLabel(self.tabview.tab("Tuner"), textvariable=self.closest_note_var)
+        self.closest_note_display = ctk.CTkLabel(self.tuner_tab, textvariable=self.closest_note_var)
         self.closest_note_display.pack()
 
-        self.max_freq_label = ctk.CTkLabel(self.tabview.tab("Tuner"), text="Max frequency:")
+        # Max frequency label
+        self.max_freq_label = ctk.CTkLabel(self.tuner_tab, text="Max frequency:")
         self.max_freq_label.pack()
-
+        # Max frequency value
         self.max_freq_var = tk.StringVar()
         self.max_freq_var.set("...")
-        self.max_freq_display = ctk.CTkLabel(self.tabview.tab("Tuner"), textvariable=self.max_freq_var)
+        self.max_freq_display = ctk.CTkLabel(self.tuner_tab, textvariable=self.max_freq_var)
         self.max_freq_display.pack()
 
-        self.closest_pitch_label = ctk.CTkLabel(self.tabview.tab("Tuner"), text="Closest pitch:")
+        # Pitch detection
+        self.closest_pitch_label = ctk.CTkLabel(self.tuner_tab, text="Closest pitch:")
         self.closest_pitch_label.pack()
-
+        # Pitch detection value
         self.closest_pitch_var = tk.StringVar()
         self.closest_pitch_var.set("...")
-        self.closest_pitch_display = ctk.CTkLabel(self.tabview.tab("Tuner"), textvariable=self.closest_pitch_var)
+        self.closest_pitch_display = ctk.CTkLabel(self.tuner_tab, textvariable=self.closest_pitch_var)
         self.closest_pitch_display.pack()
 
-        #create canvas
-        self.canvas = tk.Canvas(self.tabview.tab("Tuner"), width=150, height=150, bg="black")
+        # create canvas
+        self.canvas = tk.Canvas(self.tuner_tab, width=150, height=150, bg="black")
         self.canvas.pack()
 
+        # create arrow
         self.line = self.canvas.create_line(75, 75, 75, 25, width=2, fill="red")
 
-        self.window_samples = [0 for _ in range(WINDOW_SIZE)]
-        self.noteBuffer = ["1", "2"]
-        self.start_button = ctk.CTkButton(self.tabview.tab("Tuner"), text="Start", command=self.start)
+        # Start the audio stream button
+        self.start_button = ctk.CTkButton(self.tuner_tab, text="Start", command=self.start)
         self.start_button.pack(padx=10, pady=10)
 
-        self.stop_button = ctk.CTkButton(self.tabview.tab("Tuner"), text="Stop", command=self.stop)
+        # Stop the audio stream button
+        self.stop_button = ctk.CTkButton(self.tuner_tab, text="Stop", command=self.stop)
         self.stop_button.pack(padx=10, pady=10)
 
-
+        # Create the audio stream
+        self.window_samples = [0 for _ in range(WINDOW_SIZE)]
+        self.noteBuffer = ["1", "2"]
         self.stream = sd.InputStream(channels=1, callback=self.callback, blocksize=WINDOW_STEP, samplerate=SAMPLE_FREQ)
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        ctk.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        ctk.set_widget_scaling(new_scaling_float)
+        # scale canvas
+        self.canvas.config(width=150 * new_scaling_float, height=150 * new_scaling_float)
+        # scale arrow
+        self.canvas.coords(self.line, 75 * new_scaling_float, 75 * new_scaling_float, 75 * new_scaling_float, 25 * new_scaling_float)
+        self.scale_factor = new_scaling_float
+        self.master.geometry(f"300x{int(500 * new_scaling_float)}")
+        self.master.geometry(f"{int(300 * new_scaling_float)}x{int(500 * new_scaling_float)}")
+
+
 
     def note_to_pitch(self, note):
         """
@@ -111,7 +151,8 @@ class TunerApp:
     def update_arrow(self, angle):
         self.canvas.delete(self.line)
         x1, y1, x2, y2 = 75, 75, 75 + 50 * math.sin(math.radians(angle)), 75 - 50 * math.cos(math.radians(angle))
-        self.line = self.canvas.create_line(x1, y1, x2, y2, width=2, fill="red")
+        sf = self.scale_factor
+        self.line = self.canvas.create_line(x1 * sf, y1 * sf, x2 * sf, y2 * sf, width=2, fill="red")
 
     def calculate_angle(self, max_freq, closest_pitch):
         diff = max_freq - closest_pitch
@@ -119,6 +160,7 @@ class TunerApp:
         print(angle)
         angle = min(180, max(0, angle))
         return angle
+
     def find_closest_note(self, pitch):
         """
         This function finds the closest note for a given pitch
@@ -175,7 +217,7 @@ class TunerApp:
                 ind_end = int(OCTAVE_BANDS[j + 1] / DELTA_FREQ)
                 ind_end = ind_end if len(magnitude_spec) > ind_end else len(magnitude_spec)
                 avg_energy_per_freq = (np.linalg.norm(magnitude_spec[ind_start:ind_end], ord=2, axis=0) ** 2) / (
-                            ind_end - ind_start)
+                        ind_end - ind_start)
                 avg_energy_per_freq = avg_energy_per_freq ** 0.5
                 for i in range(ind_start, ind_end):
                     magnitude_spec[i] = magnitude_spec[i] if magnitude_spec[
@@ -222,8 +264,6 @@ class TunerApp:
         else:
             print('no input')
 
-
-
     def start(self):
         """
         Start the audio stream
@@ -238,6 +278,7 @@ class TunerApp:
         self.stream.stop()
         # self.stream.close()
         # self.stream = None
+
 
 if __name__ == "__main__":
     root = ctk.CTk()
